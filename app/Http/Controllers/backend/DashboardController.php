5c39d70a71d1c3d1e905cf1ac417fdb14c01ef2d
db-hasan <infoalihasanbd@gmail.com>
2023-12-07 19:25:52 +0600

    updt

--- app/Http/Controllers/backend/DashboardController.php
+++ app/Http/Controllers/backend/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
 use App\Models\Customar;
 use App\Models\Collection;
 use App\Models\S_order;
@@ -13,7 +14,7 @@ class DashboardController extends Controller
 {
     public function index() {
         $indexData['indexCustomar']= Customar::groupBy('customar_id')->get(); 
-        $indexData['indexWordlist']= count($indexData['indexCustomar']);  
+        $indexData['indexWordlist']= count($indexData['indexCustomar']);   
         $indexData['indexCollection']= Collection::all(); 
         $indexData['indexsOrder']= S_order::join('products', 'S_orders.product_id', '=', 'products.product_id')->get();
         return view('backend/dashboard/dashboard', $indexData);
--- resources/views/backend/dashboard/dashboard.blade.php
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -6,8 +6,8 @@
                 <div class='card p-3 text-light bg-info bg-gradient'>
                     <p><strong>Sales</strong></p>
                     <?php
-                            $totalSales= 0;
-                        ?>
+                        $totalSales= 0;
+                    ?>
                     @foreach ($indexsOrder as $order)
                         <?php
                             $total=($order->selling_price * $order->order_quantity);
@@ -20,6 +20,36 @@
             </div>
             <div class='col-md-3 p-2'>
                 <div class='card p-3 text-light bg-success bg-opacity-75'>
+                    <?php
+                        $manufacturingCost= 0;
+                    ?>
+                    @foreach ($indexsOrder as $order)
+                        <?php
+                            $total=($order->order_quantity*$order->manufacturing_cost);
+                            $manufacturingCost += $total;
+                        ?>
+                    @endforeach
+                    <p><strong>Manufacturing Cost</strong></p>
+                    <p><i class='fa-solid fa-money-check-dollar fa-2xl'></i> <strong>{{$manufacturingCost}}</strong></p>
+                    <span>Cost Amount</span>
+                </div>
+            </div>
+            <div class='col-md-3 p-2'>
+                <div class='card p-3 text-light bg-danger bg-opacity-75'>
+                    <p><strong>Expense</strong></p>
+                    <p><i class='fa-solid fa-industry fa-2xl'></i><strong>00</strong></p>
+                    <span>Total Expense</span>
+                </div>
+            </div>
+            <div class='col-md-3 p-2'>
+                <div class='card p-3 text-light' style="background-color: #007bff">
+                    <p><strong>Profit</strong></p>
+                    <p><i class='fa-solid fa-cart-flatbed-suitcase fa-2xl'></i> <strong>{{$totalSales-$manufacturingCost}}</strong></p>
+                    <span>Profit Amount</span>
+                </div>
+            </div>
+            <div class='col-md-3 p-2'>
+                <div class='card p-3 text-light bg-info bg-gradient'>
                     <?php
                             $collection= 0;
                         ?>
@@ -35,10 +65,17 @@
                 </div>
             </div>
             <div class='col-md-3 p-2'>
-                <div class='card p-3 text-light bg-warning bg-opacity-75'>
-                    <p><strong>Profit</strong></p>
+                <div class='card p-3 text-light bg-success bg-opacity-75'>
+                    <p><strong>Due Collection</strong></p>
                     <p><i class='fa-solid fa-cart-flatbed-suitcase fa-2xl'></i> <strong>{{$totalSales-$collection}}</strong></p>
-                    <span>Profit Amount</span>
+                    <span>Due Amount</span>
+                </div>
+            </div>
+            <div class='col-md-3 p-2'>
+                <div class='card p-3 text-light' style="background-color: #007bff">
+                    <p><strong>Dame Data</strong></p>
+                    <p><i class='fa-solid fa-cart-flatbed-suitcase fa-2xl'></i> <strong>00</strong></p>
+                    <span>Dame Data</span>
                 </div>
             </div>
             <div class='col-md-3 p-2'>
@@ -50,16 +87,21 @@
             </div>
         </div>
 
-        <div class="mt-3">
+        <div class="my-3">
             <div class="row">
                 <div class="col-md-8">
                     <canvas id="myBarChart"></canvas>
+                </div>
+                <div class="col-md-4 container">
+                    <canvas id="myPieChart" width="400" height="400"></canvas>
                 </div>
             </div>
         </div>
                     
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
 
     <script>
@@ -99,5 +141,49 @@
             }
         });
     </script>
+
+    <script>
+    // JavaScript to create the pie chart
+        document.addEventListener("DOMContentLoaded", function() {
+        var ctx = document.getElementById('myPieChart').getContext('2d');
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: 'My First Dataset',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+                ],
+                borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+            },
+            options: {
+            responsive: true,
+            plugins: {
+                title: {
+                display: true,
+                text: 'Custom Pie Chart'
+                }
+            }
+            }
+        });
+        });
+    </script>
+
 
 @endsection
