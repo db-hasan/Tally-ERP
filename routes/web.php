e61baf9ede576bbe01e8ab9b04e30c95aee8868c
db-hasan <infoalihasanbd@gmail.com>
2023-12-08 18:08:03 +0600

    updt

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
 });
