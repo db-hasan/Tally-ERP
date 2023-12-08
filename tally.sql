def3ccd40e86f21ddc34ff0547bb19ef2c2778ad
db-hasan <infoalihasanbd@gmail.com>
2023-12-08 15:15:13 +0600

    upsdt

--- tally.sql
+++ tally.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2023 at 08:27 AM
+-- Generation Time: Dec 08, 2023 at 10:14 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `laravel`
+-- Database: `tally`
 --
 
 -- --------------------------------------------------------
